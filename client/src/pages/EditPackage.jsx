3d0f4befa205f97eda044595af1a86f4477c5f96
akhibs <Akigbepaul555@gmail.com>
2025-12-19 07:28:02 +0100

    Update EditPackage.jsx

--- client/src/pages/EditPackage.jsx
+++ client/src/pages/EditPackage.jsx
@@ -26,7 +26,7 @@ export default function EditPackage() {
     setIsSubmit(true);
     try {
       const searchForDetails = await fetch(
-        "https://eliteload.com.ng/edit-details",
+        "http://eliteload.com.ng/edit-details",
         {
           method: "POST",
           mode: "cors",
